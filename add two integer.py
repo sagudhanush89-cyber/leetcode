5a52dbfa768c27dac7f16c698e03b220929e057f
sagudhanush89-cyber <sagudhanush89@gmail.com>
2026-07-01 13:20:16 +0530

    Add files via upload

---
+++ add two integer.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+     return num1 + num2
+
+
+if __name__ == "__main__":
+    num1 = 12
+    num2 = 5
+    result = sum
+    print("Their sum is:", result)
