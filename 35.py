318e4c8d792c9b3d5b2ab9fb7e3d5f6973289945
sagudhanush89-cyber <sagudhanush89@gmail.com>
2026-07-16 14:42:26 +0530

    Add files via upload

---
+++ 35.py
@@ -0,0 +1,8 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        nums=[1,3,5,6]
+        target=0
+        for i in nums:
+            target+=i
+            return i
+        
